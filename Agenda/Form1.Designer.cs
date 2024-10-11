94ab2aeb3b8ab2a07915731fb0f870670662dbb5
LuisangelSS <152299689+LuisangelSS@users.noreply.github.com>
2024-10-11 18:53:33 -0400

    AgendaContacts

---
+++ Agenda/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnApellido = new System.Windows.Forms.TextBox();
+            this.btnTelefono = new System.Windows.Forms.TextBox();
+            this.btnEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.dvgContactos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgContactos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // btnNombre
+            // 
+            this.btnNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNombre.Location = new System.Drawing.Point(123, 27);
+            this.btnNombre.Name = "btnNombre";
+            this.btnNombre.Size = new System.Drawing.Size(297, 29);
+            this.btnNombre.TabIndex = 1;
+            this.btnNombre.TextChanged += new System.EventHandler(this.btnNombre_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Telefono:";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido(s):";
+            // 
+            // btnApellido
+            // 
+            this.btnApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApellido.Location = new System.Drawing.Point(123, 69);
+            this.btnApellido.Name = "btnApellido";
+            this.btnApellido.Size = new System.Drawing.Size(297, 29);
+            this.btnApellido.TabIndex = 4;
+            // 
+            // btnTelefono
+            // 
+            this.btnTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTelefono.Location = new System.Drawing.Point(123, 108);
+            this.btnTelefono.Name = "btnTelefono";
+            this.btnTelefono.Size = new System.Drawing.Size(297, 29);
+            this.btnTelefono.TabIndex = 5;
+            this.btnTelefono.TextChanged += new System.EventHandler(this.btnTelefono_TextChanged);
+            // 
+            // btnEmail
+            // 
+            this.btnEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmail.Location = new System.Drawing.Point(123, 148);
+            this.btnEmail.Name = "btnEmail";
+            this.btnEmail.Size = new System.Drawing.Size(297, 29);
+            this.btnEmail.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "E-mail:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(529, 20);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(142, 49);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(529, 133);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(142, 50);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar Archivo";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(529, 75);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(142, 50);
+            this.btnBorrar.TabIndex = 11;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // dvgContactos
+            // 
+            this.dvgContactos.AllowUserToAddRows = false;
+            this.dvgContactos.AllowUserToDeleteRows = false;
+            this.dvgContactos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgContactos.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dvgContactos.Location = new System.Drawing.Point(12, 226);
+            this.dvgContactos.Name = "dvgContactos";
+            this.dvgContactos.ReadOnly = true;
+            this.dvgContactos.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dvgContactos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dvgContactos.Size = new System.Drawing.Size(659, 239);
+            this.dvgContactos.TabIndex = 12;
+            this.dvgContactos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgContactos_CellContentClick);
+            this.dvgContactos.DoubleClick += new System.EventHandler(this.dvgContactos_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 489);
+            this.Controls.Add(this.dvgContactos);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTelefono);
+            this.Controls.Add(this.btnApellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgContactos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox btnNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox btnApellido;
+        private System.Windows.Forms.TextBox btnTelefono;
+        private System.Windows.Forms.TextBox btnEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.DataGridView dvgContactos;
+    }
+}
+
---
+++ Agenda/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.CodeDom;
+
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        private List<Contacto> Contactos = new List<Contacto>();
+        private int indice = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TextWriter escribir = new StreamWriter("Agenda.txt");
+            foreach (Contacto persona in Contactos)
+            {
+                escribir.WriteLine(persona.Nombre + "|" + persona.Apellido + "|" + persona.Telefono + "|" + persona.Correo);
+            }
+            escribir.Close();
+            MessageBox.Show("Contactos Guardados");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Contacto persona = new Contacto
+            {
+                Nombre = btnNombre.Text,
+                Apellido = btnApellido.Text,
+                Telefono = btnTelefono.Text,
+                Correo = btnEmail.Text
+
+            };
+            if (indice > -1)
+            {
+                Contactos[indice] = persona;
+            }
+            else
+            {
+                Contactos.Add(persona);
+            }
+            
+            actualizaVista();
+            limpiaCampos(); 
+        }
+        private void actualizaVista()
+        {
+            dvgContactos.DataSource = null;
+            dvgContactos.DataSource = Contactos;
+            dvgContactos.ClearSelection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+               
+        }
+
+        private void btnNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dvgContactos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dvgContactos_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewRow renglon = dvgContactos.SelectedRows[0];
+            indice = dvgContactos.Rows.IndexOf(renglon);
+            Contacto persona = Contactos[indice];
+            btnNombre.Text = persona.Nombre;
+            btnApellido.Text = persona.Apellido;
+            btnTelefono.Text = persona.Telefono;
+            btnEmail.Text = persona.Correo;
+            
+        }
+        private void limpiaCampos()
+        {
+            btnNombre.Text = null;
+            btnApellido.Text=null;
+            btnTelefono.Text = null;
+            btnEmail.Text = null;
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (indice > -1)
+            {
+                Contactos.RemoveAt(indice);
+                actualizaVista();
+                limpiaCampos(); 
+                indice = -1;
+            }
+            else
+            {
+                MessageBox.Show("Seleccione el registro a borrar");
+            }
+        }
+
+    }
+}
